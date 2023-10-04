e01683c610aae94c3fd47385cdb6fd48982e8c6c
Hammad Chohan <10247@contoso.local>
2023-10-05 01:33:34 +0500

    simple-project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React  from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import {Button} from 'react-bootstrap'
+
+
+function App() {
+  return (
+    <>
+    <div>
+    <h1>Hey Developer, Wellcome To The React-laravel Project</h1>
+    <Button>Explore Project</Button>
+    </div>
+    </>
+  );
+}
+
+export default App;
